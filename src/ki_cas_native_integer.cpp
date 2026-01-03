52ea789715aa898ef37b3137f1cb195bac13f993
John Till <johndtill@gmail.com>
2026-01-03 14:24:10 +0000

    Try with int64_t

--- src/ki_cas_native_integer.cpp
+++ src/ki_cas_native_integer.cpp
@@ -132,9 +132,9 @@ size_t knownfit_str2int(std::string_view str) noexcept {
     for(const char ch : str) assert(ch >= '0' && ch <= '9');
     #endif
 
-    #if defined(_MSC_VER)
+    #if defined(_MSC_VER) && !defined(_WIN64)  // 32-bit
     // MSVC x86 implementations of std::from_chars are prone to crash when parsing size_t
-    uint64_t result;
+    int64_t result;
     const auto parse_result = std::from_chars(str.data(), str.data() + str.size(), result);
     assert(result <= std::numeric_limits<size_t>::max());
     #else
