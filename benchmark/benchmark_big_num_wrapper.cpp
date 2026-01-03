e363d2f2c661ba8574fa85e6b799053dbd3d8b38
John Till <johndtill@gmail.com>
2026-01-03 13:10:07 +0000

    Change disabled test

--- benchmark/benchmark_big_num_wrapper.cpp
+++ benchmark/benchmark_big_num_wrapper.cpp
@@ -74,18 +74,18 @@ TEST_CASE("fmpz_init_set_strview (Flint word size x3)") {
 TEST_CASE("mpz_init_set_strview (tiny)") {
     const std::string str = "1337";
 
-    BENCHMARK_ADVANCED( "mpz_init_set_strview" )(Catch::Benchmark::Chronometer meter) {
-        mpz_t big_int;
-        meter.measure([&](){mpz_init_set_strview(big_int, str);});
-        mpz_clear(big_int);
-    };
-
     // TODO: find issue
-    // BENCHMARK_ADVANCED( "mpz_set_str" )(Catch::Benchmark::Chronometer meter) {
+    // BENCHMARK_ADVANCED( "mpz_init_set_strview" )(Catch::Benchmark::Chronometer meter) {
     //     mpz_t big_int;
-    //     meter.measure([&](){mpz_init(big_int); mpz_set_str(big_int, str.c_str(), 10);});
+    //     meter.measure([&](){mpz_init_set_strview(big_int, str);});
     //     mpz_clear(big_int);
     // };
+
+    BENCHMARK_ADVANCED( "mpz_set_str" )(Catch::Benchmark::Chronometer meter) {
+        mpz_t big_int;
+        meter.measure([&](){mpz_init(big_int); mpz_set_str(big_int, str.c_str(), 10);});
+        mpz_clear(big_int);
+    };
 };
 
 TEST_CASE("mpz_init_set_strview (word size)") {
